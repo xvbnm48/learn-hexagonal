b3503eeb3d87020614cf7831440bfe92912c5062
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-07-28 22:11:45 +0700

    updater

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"github.com/xvbnm48/learn-hexagonal/internal/adapters/core/arithmetic"
+	"github.com/xvbnm48/learn-hexagonal/internal/ports"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	var core = ports.ArithmaticPort{}
+
+	core = arithmetic.NewAdapter()
+
+	// arithAdapter := arithmetic.NewAdapter()
+	// result, err := arithAdapter.Subtraction(1, 2)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	// fmt.Println(result)
+	// arithAdapter.Addition(1, 2)
+	// arithAdapter.Multiplication(1, 2)
+	// arithAdapter.Division(1, 2)
 }
--- internal/ports/core.go
+++ internal/ports/core.go
@@ -1,8 +1,8 @@
 package ports
 
 type ArithmaticPort interface {
-	Addition(a, b int) (int32, error)
-	Subtraction(a, b int) (int32, error)
-	Multiplication(a, b int) (int32, error)
-	Division(a, b int) (int32, error)
+	Addition(a, b int32) (int32, error)
+	Subtraction(a, b int32) (int32, error)
+	Multiplication(a, b int32) (int32, error)
+	Division(a, b int32) (int32, error)
 }
